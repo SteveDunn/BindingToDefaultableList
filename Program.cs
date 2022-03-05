373d3a1ad02feff27bf3bb8329654457474e550d
stevedunnhq <steve@dunnhq.com>
2022-03-05 08:09:21 +0000

    Move type into its own file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -11,41 +10,6 @@ var config = builder.Build();
 config.Bind("MyImageProcessingOptions", options);
 
 Console.WriteLine(string.Join(",", options.ResizeWidths));
-
-public class DefaultableCollection<T> : ICollection<T>
-{
-    private readonly List<T> _items;
-    private bool _overridden;
-
-    public DefaultableCollection(params T[] defaults) => _items = defaults.ToList();
-
-    public void Add(T value)
-    {
-        if (!_overridden)
-        {
-            _overridden = true;
-            _items.Clear();
-        }
-        
-        _items.Add(value);
-    }
-
-    public void Clear() => _items.Clear();
-
-    public bool Contains(T item) => _items.Contains(item);
-
-    public void CopyTo(T[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
-
-    public bool Remove(T item) => _items.Remove(item);
-
-    public int Count => _items.Count;
-    
-    public bool IsReadOnly => false;
-
-    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-}
 
 public class MyImageProcessingOptions
 {
